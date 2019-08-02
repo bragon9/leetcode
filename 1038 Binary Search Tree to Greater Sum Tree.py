3c51f17f8d23805eb78d48deba926ef2b24cc786
Brandon Spagon <bspagon@iofficecorp.com>
2019-08-02 00:50:47 -0500

    Save progress.

---
+++ 1038 Binary Search Tree to Greater Sum Tree.py
@@ -0,0 +1,20 @@
+
+class Solution(object):
+    def bstToGst(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.convert_tree(root, 0)
+        return root
+        
+        
+    def convert_tree(self, root, ans):
+        if not(root):
+            return ans
+        if root.right:
+            self.convert_tree(root.right, ans)
+        ans += root.val
+        print(root.val, ans)
+        if root.left:
+            self.convert_tree(root.left, ans)
