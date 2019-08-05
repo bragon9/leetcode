02e598bc331a88cad3f8b712f8dc856f88f4e92f
Brandon Spagon <bspagon@iofficecorp.com>
2019-08-04 21:33:29 -0500

    Save solution.

---
+++ 701 Insert into a Binary Search Tree.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        self.val = val
+        self.insert_node(root)
+        return root
+        
+    def insert_node(self, node):
+        if not(node):
+            return
+        print(node.val)
+        if self.val > node.val:
+            if node.right:
+                self.insert_node(node.right)
+            else:
+                node.right = TreeNode(self.val)
+        if self.val < node.val:
+            if node.left:
+                self.insert_node(node.left)
+            else:
+                node.left = TreeNode(self.val)
