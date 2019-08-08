0b2687932292fbb07dad05e5b177cf2588b48e5a
Brandon Spagon <bspagon@iofficecorp.com>
2019-08-08 13:31:50 -0500

    Remove unnecessary lines.

--- 617 Merge Two Binary Trees.py
+++ 617 Merge Two Binary Trees.py
@@ -5,12 +5,6 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        if not(t1):
-            return t2
-        self.merge(t1, t2)
-        return t1
-    
-    def merge(self, t1, t2):
         # If no nodes, return None.
         if not(t1) and not(t2):
             return None
@@ -20,8 +14,8 @@ class Solution(object):
         if not(t1) and t2:
             return t2
         # Set the children recursively.
-        t1.left = self.merge(t1.left, t2.left)
-        t1.right = self.merge(t1.right, t2.right)
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
         # There are two nodes. Set the new value and return the node.
         t1.val += t2.val
         return t1
