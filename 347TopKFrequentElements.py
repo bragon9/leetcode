01663985096484e37f21a26e88bbfc8ef5610bc0
bragon9 <eleventy07@yahoo.com>
2020-07-17 12:31:30 -0500

    Add heap solution.

--- 347TopKFrequentElements.py
+++ 347TopKFrequentElements.py
@@ -1,7 +1,17 @@
-from collections import defaultdict
+#from collections import defaultdict
+import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_dict = {}
+        for num in nums:
+            if num in nums_dict:
+                nums_dict[num] += 1
+            else:
+                nums_dict[num] = 1
+        return heapq.nlargest(k, nums_dict, lambda x: nums_dict[x])
+        
+        ''' Old Answer
         count_dict = defaultdict(int)
         ans = []
         # Loop through and get occurrence count.
@@ -14,5 +24,6 @@ class Solution:
             ans.append(num)
             if k_ptr >= k:
                 break
-        return ans
+        return ans 
+        '''
     
