11192303cb8c4f41589842a2414d02c2d9cf9f6d
bragon9 <eleventy07@yahoo.com>
2020-12-01 09:48:25 -0600

    Add iterative solution.

--- 104MaximumDepthofBinaryTree.py
+++ 104MaximumDepthofBinaryTree.py
@@ -1,9 +1,29 @@
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+# class Solution(object):
+#     def maxDepth(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: int
+#         """
+#         if not(root):
+#             return 0
+#         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+    
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
         if not(root):
             return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        ans = 0
+        to_visit = [[root]]
+        while to_visit:
+            ans += 1
+            level = to_visit.pop()
+            new_level = []
+            for node in level:
+                if node.left:
+                    new_level.append(node.left)
+                if node.right:
+                    new_level.append(node.right)
+            if new_level:
+                to_visit.append(new_level)
+        return ans
