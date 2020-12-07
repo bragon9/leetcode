4d406749533425cc156c6283fe8fecd91b539a5a
bragon9 <eleventy07@yahoo.com>
2020-12-07 17:37:03 -0600

    Save solution.

---
+++ 59SpiralMatrixII.py
@@ -0,0 +1,32 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        ans = []
+        for i in range(n):
+            ans.append([0] * n)
+        ptr = 1
+        x = -1
+        y = 0
+        offset = 0
+        while ptr <= n**2:
+            for i in range(n-(offset*2)):
+                x += 1
+                if ans[y][x] == 0:
+                    ans[y][x] = ptr
+                ptr += 1
+            for i in range(n-(offset*2)-1):
+                y += 1
+                if ans[y][x] == 0:
+                    ans[y][x] = ptr
+                ptr += 1
+            for i in range(n-(offset*2)-1):
+                x -= 1
+                if ans[y][x] == 0:
+                    ans[y][x] = ptr
+                ptr += 1
+            for i in range(n-(offset*2)-2):
+                y -= 1
+                if ans[y][x] == 0:
+                    ans[y][x] = ptr
+                ptr += 1
+            offset += 1
+        return ans
