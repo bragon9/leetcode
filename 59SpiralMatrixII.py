c79bb2406779f7363eb605960bff5e9bc9a106c4
bragon9 <eleventy07@yahoo.com>
2020-12-07 17:39:30 -0600

    Increment offset by 2.

--- 59SpiralMatrixII.py
+++ 59SpiralMatrixII.py
@@ -8,25 +8,26 @@ class Solution:
         y = 0
         offset = 0
         while ptr <= n**2:
-            for i in range(n-(offset*2)):
+            for i in range(n-offset):
                 x += 1
                 if ans[y][x] == 0:
                     ans[y][x] = ptr
                 ptr += 1
-            for i in range(n-(offset*2)-1):
+            for i in range(n-offset-1):
                 y += 1
                 if ans[y][x] == 0:
                     ans[y][x] = ptr
                 ptr += 1
-            for i in range(n-(offset*2)-1):
+            for i in range(n-offset-1):
                 x -= 1
                 if ans[y][x] == 0:
                     ans[y][x] = ptr
                 ptr += 1
-            for i in range(n-(offset*2)-2):
+            for i in range(n-offset-2):
                 y -= 1
                 if ans[y][x] == 0:
                     ans[y][x] = ptr
                 ptr += 1
-            offset += 1
+            offset += 2
         return ans
+            
